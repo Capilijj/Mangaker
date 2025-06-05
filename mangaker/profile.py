33ce50f7017756ec01e9ad57ede6b107320fbd2d
Capili, Justine James R. <justinejamesrcapili@iskolarngbayan.pup.edu.ph>
2025-06-05 08:28:17 +0800

    profile page ahron dito ka mag lagay ng ui

--- mangaker/profile.py
+++ mangaker/profile.py
@@ -3,5 +3,5 @@ import customtkinter as ctk
 class ProfilePage(ctk.CTkFrame):
     def __init__(self, parent):
         super().__init__(parent)
-        label = ctk.CTkLabel(self, text="👤 inamoProfile Page", font=ctk.CTkFont(size=24))
+        label = ctk.CTkLabel(self, text="👤 Profile Page", font=ctk.CTkFont(size=24))
         label.pack(pady=50)
