ab7854c482bf5e84f63adffcc9db4220fba3ca3a
Kenneth James Boncales <kennethboncales608@gmail.com>
2025-07-03 07:33:23 +0800

    fix: add latest update to db.

--- users_db.py
+++ users_db.py
@@ -3,6 +3,7 @@ current_session = {
     "email": None,
     "username": None,
     "user_id": None,
+    "role": "user",  # default role, can be 'admin' or 'user'
     "profile_image": None,
     "created_at": None  # optional, if you want to store timestamp
 }
